5fddd6bab361989fcbe704ce0c286ee0798d1def
Rafaell Lycan <sonny.webdsg@gmail.com>
2016-12-18 15:53:36 +0100

    Add routes and resource controllers

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,13 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/', function () {
+    return response()->json(['message' => 'Jobs API', 'status' => 'Connected']);;
+});
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::resource('jobs', 'JobsController');
+Route::resource('companies', 'CompaniesController');
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:api');
